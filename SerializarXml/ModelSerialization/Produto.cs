493df8bf5c31af4fac442ffc41cd4210f71daa56
OGEA\cristiano.souza <cristiano.souza@ogea.com.br>
2025-09-29 12:47:15 -0300

    Initial

---
+++ SerializarXml/ModelSerialization/Produto.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace SerializarXml.ModelSerialization
+{
+    public class Produto
+    {
+        public string cProd { get; set; }
+        public string cEAN { get; set; }
+        public string xProd { get; set; }
+        public string NCM { get; set; }
+        public string CFOP { get; set; }
+        public string uCom { get; set; }
+        public double qCom { get; set; }
+        public double vUnCom { get; set; }
+    }
+}
